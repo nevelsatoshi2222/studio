f354544f2a2dd7bec98b53bf09a9a925a4878b2a
MITESH PATEL <nevelsatoshi2222@gmail.com>
2025-10-19 07:16:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "public-governance-859029-c316e",
+  "appId": "1:826113692111:web:70f966e4dc292f1ae6c2ba",
+  "apiKey": "AIzaSyCTD57QBNRjULEDM4qZoQ3a86anTU8XcF8",
+  "authDomain": "public-governance-859029-c316e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "826113692111"
+};
